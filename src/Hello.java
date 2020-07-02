b44db13eb9dd659f2d469598101cd8c3af80e738
moonpeter <moonpeter@kakao.com>
2020-07-02 12:34:20 +0900

    update

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, /* 이것은 주석 아님 */ world!");
+		System.out.println("Hello, world!// 이것도 주석 아님");		
+	}
+
+}
---
+++ src/ch02/Ex2_12.java
@@ -0,0 +1,13 @@
+package ch02;
+
+public class Ex2_12 {
+	public static void main(String argString []) {
+		String string = "3";
+		
+		System.out.println(string.charAt(0) - '0');
+		System.out.println('3' - '0' +1);
+		System.out.println(Integer.parseInt("3") + 1);
+		System.out.println("3" + 1);
+		System.out.println((char)(3+ '0'));
+	}
+}
