eec48849492843963bf9fd16be346a66a3ee6e80
moonpeter <kyoungho.moon@purplworks.co.kr>
2020-07-07 10:26:05 +0900

    page 110

--- src/ch04/Ex4_5.java
+++ src/ch04/Ex4_5.java
@@ -1,7 +1,36 @@
 package ch04;
 
+import java.util.Scanner;
+
 public class Ex4_5 {
 	public static void main(String args[]) {
-		wefwefwefwef
+		int score = 0;
+		char grade = ' ', opt = '0';
+		
+		System.out.print("점수를 입력해주세요.>");
+		
+		Scanner scanner = new Scanner(System.in);
+		score = scanner.nextInt(); 		// 화면을 통해 입력받은 점수를 score에 저장
+		
+		System.out.printf("당신의 점수는 %d입니다.%n", score);
+		
+		if (score >= 90) {
+			grade = 'A';
+			if (score >= 98) {
+				opt = '+';
+			} else if  (score < 94) {
+				opt = '-';
+			}
+		} else if (score >= 80) {
+			grade = 'B';
+			if (score >= 88) {
+				opt = '+';
+			} else if (score < 84) {
+				opt = '-';
+			}
+		} else {
+			grade = 'C';
+		}
+		System.out.printf("당신의 학점은 %c%c입니다.%n", grade, opt);
 	}
 }
