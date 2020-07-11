6e2deb1397da7ba55bf8ec6a4e182340bebc2ef7
moonpeter <moonpeter@kakao.com>
2020-07-11 11:43:45 +0900

    update

---
+++ src/ch07/Ex7_6.java
@@ -0,0 +1,14 @@
+package ch07;
+
+import static java.lang.System.out;
+import static java.lang.Math.*;
+
+public class Ex7_6 {
+	public static void main(String[] args) {
+		// system.out.println(Math.random());
+		out.println(random());
+		
+		// system.out.println("Math.PI :"+Math.PI);
+		out.println("Math.PI :" + PI);
+	}
+}
